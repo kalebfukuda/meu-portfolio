8ac64add80504078744ae18f8ba644749309d49e
kalebfukuda <kalebfukudajp@gmail.com>
2025-09-05 09:21:00 +0900

    Removed bootstrap and added tailwind

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import bannerImg from '../assets/images/banner_port.jpg';
+import './Banner.css';
+const Banner = () => {
+  return (
+      <div className="banner">
+      </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../components/Banner';
+
+function Home() {
+  return (
+    <div>
+      <Banner />
+      <h1>Welcome to my portfolio</h1>
+      <p>This is my home page.</p>
+    </div>
+  );
+}
+
+export default Home;
