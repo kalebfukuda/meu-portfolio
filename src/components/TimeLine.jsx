113e3c5bdd43c28bb68c5cfe602cba85f130a82c
kalebfukuda <kalebfukudajp@gmail.com>
2025-09-10 20:01:27 +0900

    Solved part of the problem in personal section

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -74,9 +74,9 @@ const TimeLine = () => {
 
   return (
     <div ref={timeLineRef}>
-      <h1 className="text-8xl font-bold text-accent mb-15 text-center sectionHeader">
-        A LITTLE OF MY HISTORY...
-      </h1>
+      <p className="text-2xl md:text-5xl font-bold text-accent mb-1 md:mb-12 mt-2 md:mt-0 text-center sectionHeader">
+        MY HISTORY...
+      </p>
 
       <div
         ref={boxRef}
@@ -86,128 +86,139 @@ const TimeLine = () => {
         <PersonalCard name={companyName} />
       </div>
 
-      <div>
-        <ol class="items-center sm:flex ">
-          <li class="relative mb-4 sm:mb-0">
-            <div
-              class="flex items-center timeLineBullet"
-              onClick={() => bulletClick("Geologistica")}
-            >
-              <div class="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
-              <div class="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
-            </div>
-            <div class="mt-3 sm:pe-12 min-w-xs contentTimeline">
-              <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                February, 2018
-              </time>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-location-dot"></i> São Paulo, Brazil
-              </p>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-laptop"></i> Full-stack Developer
-              </p>
-              <a
-                className="text-greener"
-                target="_ref"
-                href="https://www.geologistica.com.br/"
+      <div className>
+        <ol class="items-center flex flex-col md:flex-row">
+          <li classd="relative mb-6 sm:mb-0">
+            <div className="flex lg:grid">
+              <div
+                className="flex items-center timeLineBullet"
+                onClick={() => bulletClick("Geologistica")}
               >
-                Geologistica
-              </a>
+                <div className="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
+                <div className="hidden sm:flex w-full bg-lightBlue h-0.5"></div>
+              </div>
+              <div className="mt-3 pe-1 md:pe-12 min-w-xs ps-2 md:ps-0 contentTimeline">
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  February, 2018
+                </time>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-location-dot"></i> São Paulo, Brazil
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-laptop"></i> Full-stack Developer
+                </p>
+                <a
+                  className="text-greener"
+                  target="_ref"
+                  href="https://www.geologistica.com.br/"
+                >
+                  Geologistica
+                </a>
+              </div>
             </div>
           </li>
-          <li class="relative mb-6 sm:mb-0">
-            <div
-              class="flex items-center timeLineBullet"
-              onClick={() => bulletClick("Sinqia (Evertec Brasil)")}
-            >
-              <div class="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
-              <div class="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
-            </div>
-            <div class="mt-3 sm:pe-12 min-w-xs contentTimeline">
-              <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                July, 2021
-              </time>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-location-dot"></i> São Paulo, Brazil
-              </p>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-laptop"></i> Full-stack Developer
-              </p>
-              <a target="_ref" href="https://evertecinc.com/pt-br/">
-                Sinqia (Evertec Brasil)
-              </a>
-            </div>
-          </li>
-          <li class="relative mb-6 sm:mb-0">
-            <div
-              class="flex items-center timeLineBullet"
-              onClick={() => bulletClick("Self employee")}
-            >
-              <div class="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
-              <div class="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
-            </div>
-            <div class="mt-3 sm:pe-12 min-w-xs contentTimeline">
-              <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                August, 2023
-              </time>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-location-dot"></i> Kawasaki, Japan
-              </p>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-bolt"></i> Electrician
-              </p>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                Self employee
-              </p>
-            </div>
-          </li>
-          <li class="relative mb-6 sm:mb-0">
-            <div
-              class="flex items-center timeLineBullet"
-              onClick={() => bulletClick("Le Wagon Tokyo")}
-            >
-              <div class="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
-              <div class="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
-            </div>
-            <div class="mt-3 sm:pe-12 min-w-xs contentTimeline">
-              <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                July, 2025
-              </time>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-location-dot"></i> Tokyo, Japan
-              </p>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-graduation-cap"></i> AI Web Development
-              </p>
-              <a
-                target="_ref"
-                href="https://www.lewagon.com/tokyo?gad_source=1&gad_campaignid=9578090157&gbraid=0AAAAADDMI2hSBxbJxMnbNEYoWz1kyHtkJ&gclid=Cj0KCQjwoP_FBhDFARIsANPG24N2DI4l8_RFWR75x4lWr02ROQNo8VPD6P9dThPg2FOtHc05mtf3_38aAsJqEALw_wcB"
+          <li className="relative mb-6 sm:mb-0">
+            <div className="flex md:grid">
+              <div
+                className="flex items-center timeLineBullet"
+                onClick={() => bulletClick("Sinqia (Evertec Brasil)")}
               >
-                Le Wagon Tokyo
-              </a>
+                <div className="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
+                <div className="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
+              </div>
+              <div className="mt-3 pe-1 md:pe-12 min-w-xs ps-2 md:ps-0 contentTimeline">
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  July, 2021
+                </time>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-location-dot"></i> São Paulo, Brazil
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-laptop"></i> Full-stack Developer
+                </p>
+                <a target="_ref" href="https://evertecinc.com/pt-br/">
+                  Sinqia (Evertec Brasil)
+                </a>
+              </div>
             </div>
           </li>
-          <li class="relative mb-6 sm:mb-0">
-            <div
-              class="flex items-center timeLineBullet"
-              onClick={() => bulletClick("Loading...")}
-            >
-              <div class="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
-              <div class="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
+          <li className="relative mb-6 sm:mb-0">
+            <div className="flex md:grid">
+              <div
+                className="flex items-center timeLineBullet"
+                onClick={() => bulletClick("Self employee")}
+              >
+                <div className="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
+                <div className="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
+              </div>
+              <div className="mt-3 pe-1 md:pe-12 min-w-xs ps-2 md:ps-0 contentTimeline">
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  August, 2023
+                </time>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-location-dot"></i> Kawasaki, Japan
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-bolt"></i> Electrician
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  Self employee
+                </p>
+              </div>
             </div>
-            <div class="mt-3 contentTimeline">
-              <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                September, 2025
-              </time>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-spinner fa-spin"></i> Loading...
-              </p>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-spinner fa-spin"></i> Loading...
-              </p>
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                <i class="fa-solid fa-spinner fa-spin"></i> Loading...
-              </p>
+          </li>
+          <li className="relative mb-6 sm:mb-0">
+            <div className="flex md:grid">
+              <div
+                className="flex items-center timeLineBullet"
+                onClick={() => bulletClick("Le Wagon Tokyo")}
+              >
+                <div className="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
+                <div className="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
+              </div>
+              <div className="mt-3 pe-1 md:pe-12 min-w-xs ps-2 md:ps-0 contentTimeline">
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  July, 2025
+                </time>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-location-dot"></i> Tokyo, Japan
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-graduation-cap"></i> AI Web
+                  Development
+                </p>
+                <a
+                  target="_ref"
+                  href="https://www.lewagon.com/tokyo?gad_source=1&gad_campaignid=9578090157&gbraid=0AAAAADDMI2hSBxbJxMnbNEYoWz1kyHtkJ&gclid=Cj0KCQjwoP_FBhDFARIsANPG24N2DI4l8_RFWR75x4lWr02ROQNo8VPD6P9dThPg2FOtHc05mtf3_38aAsJqEALw_wcB"
+                >
+                  Le Wagon Tokyo
+                </a>
+              </div>
+            </div>
+          </li>
+          <li className="relative mb-6 sm:mb-0">
+            <div className="flex md:grid">
+              <div
+                className="flex items-center timeLineBullet"
+                onClick={() => bulletClick("Loading...")}
+              >
+                <div className="z-10 flex items-center justify-center w-3 h-3 bg-accent rounded-full shrink-0 timelineBulletPoint"></div>
+                <div className="hidden sm:flex w-full bg-lightBlue h-0.5 dark:bg-gray-700"></div>
+              </div>
+              <div className="mt-3 pe-1 md:pe-12 min-w-xs ps-2 md:ps-0 contentTimeline">
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  September, 2025
+                </time>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-spinner fa-spin"></i> Loading...
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-spinner fa-spin"></i> Loading...
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <i className="fa-solid fa-spinner fa-spin"></i> Loading...
+                </p>
+              </div>
             </div>
           </li>
         </ol>
